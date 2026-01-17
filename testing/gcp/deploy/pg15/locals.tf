b20b83a5cfd0b007894570574c4b727595d53eb7
justin <justin@randoneering.tech>
2026-01-16 20:22:59 -0700

    updating for gcp

--- testing/gcp/deploy/pg15/locals.tf
+++ testing/gcp/deploy/pg15/locals.tf
@@ -1,9 +1,8 @@
 locals {
-  project_id       = ""  # Set your GCP project ID
+  project_id       = "165700873236"  # Set your GCP project ID
   region           = "us-central1"
   instance_name    = "pgfirstaid-pg15"
   postgres_version = "POSTGRES_15"
   database_name    = "pgFirstAid"
   db_user          = "randoneering"
-  personal_ip      = "0.0.0.0/0"  # Replace with your IP or CIDR range
 }

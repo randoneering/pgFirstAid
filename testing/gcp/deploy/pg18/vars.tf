ad5c1e1c8f0881552350e0014382a4698c537a66
justin <justin@randoneering.tech>
2026-01-19 11:12:54 -0700

    further updates to gcp

--- testing/gcp/deploy/pg18/vars.tf
+++ testing/gcp/deploy/pg18/vars.tf
@@ -1,4 +1,4 @@
 variable "personal_ip" {
-  description ="Personal IP to allow access to the instance"
-  type = string
+  description = "Personal IP to allow access to the instance"
+  type        = string
 }

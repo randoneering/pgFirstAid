68e7a92d92ed29a09d0899141fe4bcd4b950b785
justin <justin@randoneering.tech>
2026-01-19 10:21:36 -0700

    updating gcp modules and deploy folders

--- testing/gcp/deploy/pg18/locals.tf
+++ testing/gcp/deploy/pg18/locals.tf
@@ -1,9 +1,8 @@
 locals {
-  project_id       = ""  # Set your GCP project ID
-  region           = "us-central1"
+  project_id       = "165700873236"  # Set your GCP project ID
+  region           = "us-west1"
   instance_name    = "pgfirstaid-pg18"
   postgres_version = "POSTGRES_18"
   database_name    = "pgFirstAid"
   db_user          = "randoneering"
-  personal_ip      = "0.0.0.0/0"  # Replace with your IP or CIDR range
 }

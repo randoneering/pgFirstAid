a5ce3f353ba3a3f52b33b2bf9606fcf872f9da9f
randoneering <justin@randoneering.tech>
2026-04-29 03:01:04 +0000

    fix(action): slow action creating managed view

--- testing/pgTAP/99_teardown.sql
+++ testing/pgTAP/99_teardown.sql
@@ -1,5 +1,7 @@
 -- Session-level pgTAP teardown for Python harness
 DROP SCHEMA IF EXISTS pgfirstaid_test CASCADE;
+SET lock_timeout = '30s';
 DROP VIEW IF EXISTS v_pgfirstaid;
+RESET lock_timeout;
 DROP FUNCTION IF EXISTS pg_firstAid();
 DROP FUNCTION IF EXISTS _pg_firstaid_checkpoint_stats();
